646def52e5e666ccdbaa2be1fb026026e0d08169
Mikifor <mikifor.astro@gmail.com>
2023-07-29 14:42:54 +0500

    - treating

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import reportWebVitals from './reportWebVitals'
-import TimerOne from './timerOne/App'               //UI первого таймера
-import storeOne from "./timerOne/State/state"       //BLL первого таймера
-import TimerTwo from './timerTwo/App'               //UI второго таймера
-import storeTwo from "./timerTwo/State/state"       //BLL второго таймера
+//import TimerOne from './timerOne/App'               //UI первого таймера
+//import storeOne from "./timerOne/State/state"       //BLL первого таймера
+//import TimerTwo from './timerTwo/App'               //UI второго таймера
+//import storeTwo from "./timerTwo/State/state"       //BLL второго таймера
 import TimerThree from './timerThree/App'           //UI третьего таймера
 import storeThree from "./timerThree/State/state"   //BLL третьего таймера
 
@@ -20,13 +20,13 @@ const rerenderDOM = () => {root.render(
     <TimerThree /*<-UI таймера */ store={storeThree} /*<-BLL таймера */ />                    
   </React.StrictMode>
 )
-};
+}
 
-rerenderDOM();
+rerenderDOM()
 
 storeThree.observer(rerenderDOM); /*<-BLL таймера */
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals()
